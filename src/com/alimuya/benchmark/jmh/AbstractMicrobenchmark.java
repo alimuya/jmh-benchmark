1ffb1ae60187db99f1a571b2f36f62e85d80b95c
lixin <alimuya@126.com>
2015-07-03 01:09:02 +0800

    chart

--- src/com/alimuya/benchmark/jmh/AbstractMicrobenchmark.java
+++ src/com/alimuya/benchmark/jmh/AbstractMicrobenchmark.java
@@ -2,6 +2,14 @@ package com.alimuya.benchmark.jmh;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.Test;
@@ -13,11 +21,17 @@ import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.results.Result;
+import org.openjdk.jmh.results.RunResult;
 import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+/**
+ * @author ov_alimuya
+ *
+ */
 @Warmup(iterations = AbstractMicrobenchmark.DEFAULT_WARMUP_ITERATIONS)
 @Measurement(iterations = AbstractMicrobenchmark.DEFAULT_MEASURE_ITERATIONS)
 @Fork(AbstractMicrobenchmark.DEFAULT_FORKS)
@@ -25,15 +39,14 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 @BenchmarkMode(Mode.All)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 public abstract class AbstractMicrobenchmark {
- 
-    protected static final int DEFAULT_WARMUP_ITERATIONS = 10;
-    protected static final int DEFAULT_MEASURE_ITERATIONS = 10;
-    protected static final int DEFAULT_FORKS = 2;
+    protected static final int DEFAULT_WARMUP_ITERATIONS = 20;
+    protected static final int DEFAULT_MEASURE_ITERATIONS = 20;
+    protected static final int DEFAULT_FORKS = 5;
  
     protected static final String[] JVM_ARGS = {
         "-server","-Xms768m", "-Xmx768m",
         "-XX:MaxDirectMemorySize=768m", "-XX:+AggressiveOpts", "-XX:+UseBiasedLocking",
-        "-XX:+UseFastAccessorMethods", "-XX:+UseStringCache", "-XX:+OptimizeStringConcat",
+        "-XX:+UseFastAccessorMethods", "-XX:+OptimizeStringConcat",
         "-XX:+HeapDumpOnOutOfMemoryError",
     };
  
@@ -43,15 +56,18 @@ public abstract class AbstractMicrobenchmark {
         ChainedOptionsBuilder runnerOptions = new OptionsBuilder()
             .include(".*" + className + ".*")
             .resultFormat(ResultFormatType.JSON)
+            .threads(Runtime.getRuntime().availableProcessors())
             .result(filePath())
             .jvmArgs(JVM_ARGS);
-        new Runner(runnerOptions.build()).run();
+        Collection<RunResult> results = new Runner(runnerOptions.build()).run();
+        System.out.println("");
+        this.chart(results);
     }
     
     
     private String filePath() throws IOException{
     	File root=new File("");
-    	String filePath=root.getAbsolutePath()+"/result/"+this.getClass().getName()+".json";
+    	String filePath=root.getAbsolutePath()+"/result/"+this.getClass().getName()+"_"+System.currentTimeMillis()+".json";
     	File file = new File(filePath);
         if (file.exists()) {
             file.delete();
@@ -61,5 +77,59 @@ public abstract class AbstractMicrobenchmark {
         }
         return filePath;
     }
+    
+    protected abstract String benchmarkName();
+    
+    
+    private Map<String,Long> getcharMap(Collection<RunResult> results){
+    	Map<String,Long> map=new HashMap<String,Long>();
+    	 for (RunResult result :results) {
+ 			Result<?> tmp = result.getPrimaryResult();
+ 			String key=tmp.getLabel();
+ 			double score=tmp.getScore();
+ 			BigDecimal   b   =   new   BigDecimal(score);  
+ 			long value   = (long)((b.setScale(5,   BigDecimal.ROUND_HALF_UP).doubleValue())*100000);
+ 			map.put(key, value);
+ 		}
+    	return map;
+    }
+    
+    private void chart (Collection<RunResult> results) {
+    	String title=this.benchmarkName();
+    	Map <String,Long>  resultMap=this.getcharMap(results);
+		Comparator<Entry<String, Long>> comparator = new Comparator<Entry<String, Long>>() {
+			public int compare (Entry<String, Long> o1, Entry<String, Long> o2) {
+				return (int)(o1.getValue() - o2.getValue());
+			}
+		};
+		ArrayList<Entry<String, Long>> list = new ArrayList<Entry<String, Long>>(resultMap.entrySet());
+		Collections.sort(list, comparator);
+
+		StringBuilder names = new StringBuilder(512);
+		StringBuilder times = new StringBuilder(512);
+		long max = 0;
+		int count = 0;
+		for (Entry<String, Long> entry : list) {
+			String name = entry.getKey();
+			names.insert(0, '|');
+			names.insert(0, name);
+			long time = entry.getValue();
+			times.append(time);
+			times.append(',');
+			max = Math.max(max, time);
+			count++;
+		}
+		times.setLength(times.length() - 1);
+		names.setLength(names.length() - 1);
+		int height = count * 18 + 21;
+		int width = Math.min(700, 300000 / height);
+		System.out.println("-------------------------------------------");
+		System.out.println("GOOGLE CHART : ");
+		System.out.println("");
+		System.out.println("http://chart.apis.google.com/chart?chtt=" + title + "&" + "chs=" + width + "x" + height
+			+ "&chd=t:" + times + "&chds=0," + max + "&chxl=0:|" + names + "&cht=bhg&chbh=10&chxt=y&"
+			+ "chco=660000|660033|660066|660099|6600CC|6600FF|663300|663333|"
+			+ "663366|663399|6633CC|6633FF|666600|666633|666666");
+	}
  
 }
